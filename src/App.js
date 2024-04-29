3033e154ff2686094438cc15e6d234b4b15486a9
damir <delewev.damir@mail.ru>
2024-04-29 18:42:18 +0300

    js ino

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import { BrowserRouter } from "react-router-dom";
+import { Provider } from "react-redux";
+import { store } from "./stores/store";
+import Router from "./pages/Router";
+
+function App() {
+  return (
+    <Provider store={store}>
+      <BrowserRouter>
+        <Router></Router>
+      </BrowserRouter>
+    </Provider>
+  );
+}
+
+export default App;
